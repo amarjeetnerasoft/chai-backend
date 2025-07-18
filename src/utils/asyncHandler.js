7430744faddb0b1825cee8f839886bc36583cd38
Amarjeet Kumar <amarjeetkumar.nerasoft@gmail.com>
2025-07-18 19:01:52 +0530

    cloudinary2

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,16 +1,16 @@
-const asyncHandler = (requestHandler) =>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
-export {asyncHandler}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
 
 // const asyncHandler = ()=>{}
 // const asyncHandler = (func)=> async()=>{}
 
 // const asyncHandler =(fn)=> async(req,res,next)=>{
 //     try {
-//        await fn(req,res,next) 
+//        await fn(req,res,next)
 //     } catch (error) {
 //         res.status(error.code || 500).json({
 //             message:error.message
