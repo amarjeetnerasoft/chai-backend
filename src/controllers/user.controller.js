b07b79073a0db4d6d1508ad0a38ebd7c61459a66
Amarjeet Kumar <amarjeetkumar.nerasoft@gmail.com>
2025-07-22 10:40:54 +0530

    refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -208,37 +208,43 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     throw new ApiError(401, "Unauthorized request");
   }
 
-  const decodedToken = jwt.verify(
-    incomingRefreshToken,
-    process.env.REFRESH_TOKEN_SECRET
-  )
-
-  const user = await User.findById(decodedToken?._id)
-
-  if(!user){
-    throw new ApiError(401, "Invalid refresh token")
-  }
-
-  if(incomingRefreshToken !==user?.refreshToken){
-    throw new ApiError(401,"Refresh token is expired or used")
-  }
-
-  const options ={
-    httpOnly:true,
-    secure: true
-  }
-
- const {accessToken, refreshToken} =  await generateAccessAndRefereshTokens(user._id)
-
-  return res
-  .status(200)
-  .cookie("accessToken",accessToken,options)
-  .cookie("refreshToken", refreshToken,options)
-  .json(
-    new ApiResponse(
-      200
-    )
-  )
+ try {
+   const decodedToken = jwt.verify(
+     incomingRefreshToken,
+     process.env.REFRESH_TOKEN_SECRET
+   )
+ 
+   const user = await User.findById(decodedToken?._id)
+ 
+   if(!user){
+     throw new ApiError(401, "Invalid refresh token")
+   }
+ 
+   if(incomingRefreshToken !==user?.refreshToken){
+     throw new ApiError(401,"Refresh token is expired or used")
+   }
+ 
+   const options ={
+     httpOnly:true,
+     secure: true
+   }
+ 
+  const {accessToken, refreshToken} =  await generateAccessAndRefereshTokens(user._id)
+ 
+   return res
+   .status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken", refreshToken,options)
+   .json(
+     new ApiResponse(
+       200,
+       {accessToken, refreshToken, newRefreshToken},
+       "Access token refreshed"
+     )
+   )
+ } catch (error) {
+  throw new ApiError(401, error?.message || "Invalid rerefresh token")
+ }
 })
 
-export { registerUser, loginUser, logoutUser };     
+export { registerUser, loginUser, logoutUser ,refreshAccessToken};     
